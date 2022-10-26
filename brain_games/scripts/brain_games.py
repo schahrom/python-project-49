0d2082eb9003cb4a802a6b8d7eaa4159c6f7970c
schahrom <schahrom1232939@gmail.com>
2022-10-26 12:57:24 +0300

    Adding functionality in the form of a prompt for entering a name and greeting

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
+import prompt
+from cli import welcome_user
 def main():
     print('Welcome to the Brain Games!')
 
 
 if __name__ == '__main__':
     main()
+
+name = prompt.string('May I have your name?') #Приглашение пользлвателя к вводу имени
+welcome_user(name) #Вызов функции реализующей приветствие с пользователем
